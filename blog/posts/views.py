c614b08c614097dabcd677cd34d57d592dd7912a
aditya <adityakmr9672@gmail.com>
2019-03-02 21:44:11 +0530

    posts filter to ten posts on home page only

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render, get_object_or_404
-#class based view
-from django.views.generic import ListView, DetailView
-#from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 #from django.contrib.auth.models import User
 # Create your views here.posts = [
+from django.shortcuts import redirect
+
+#class based view
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = 'posts/home.html'
+    template_name = 'posts/posts.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Post.objects.all()[:10]
+        else : 
+            return super().get_queryset()
 
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin,DetailView):
     model = Post
     template_name = 'posts/post_detail.html'
