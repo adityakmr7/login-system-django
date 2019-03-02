9170446b2bdacf5ef59253d974d5eaedc7fd02cc
aditya <adityakmr9672@gmail.com>
2019-03-02 22:14:09 +0530

    some bug fixes

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -9,7 +9,7 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = 'posts/posts.html'
+    template_name = 'posts/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
     
