18456545b50591dd7bff9977597746892ce84388
aditya <adityakmr9672@gmail.com>
2019-03-10 22:39:50 +0530

    detail page ui imporoved

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.urls import path, include
-from .views import PostView, PostDetailView
+from .views import PostView, PostDetailView, CategoryDetailView
 
 urlpatterns = [
     path('', PostView.as_view(), name='posts-home'),
     path('<slug>', PostDetailView.as_view(), name='posts-detail'),
+    path('category/<slug>', CategoryDetailView.as_view(), name='category-detail'),
+
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -13,16 +13,21 @@ class PostView(ListView):
    context_object_name = 'all_categs'
 
    def get_queryset(self):
-      if not self.request.user.is_authenticated:        
-         return Category.objects.all()[0:6]
-      else:
+      if self.request.user.is_authenticated:        
          return Category.objects.all()
+      else:
+         return Category.objects.all()[:6]
    
    def get_context_data(self):
-      context = super(PostView, self).get_context_data()
-      context['latest_posts'] = Post.objects.order_by('-date_posted')[0:8]
-      return context
-
+      if not self.request.user.is_authenticated:           
+         context = super(PostView, self).get_context_data()
+         context['latest_posts'] = Post.objects.order_by('-date_posted')[0:6]
+         return context
+      else:
+         context = super(PostView, self).get_context_data()
+         context['latest_posts'] = Post.objects.order_by('-date_posted')
+         return context
+         
    # def get_success_url(self):
    #     return reverse('home') #add your path
 
@@ -30,3 +35,14 @@ class PostView(ListView):
 class PostDetailView(LoginRequiredMixin,DetailView):
     model = Post
     template_name = 'posts/post_detail.html'
+
+   
+
+class CategoryDetailView(LoginRequiredMixin, DetailView):
+   model = Category
+   template_name = 'posts/category_detail.html'
+
+   # def get_context_data(self):
+   #    context = super(PostView, self).get_context_data()
+   #    context['posts'] = Post.objects.order_by('-date_posted')
+   #    return context
